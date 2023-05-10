261776f0f19a82d4cf8722152ddf0ebf8e98e1ad
Bangseungjae <tmdwo5825@naver.com>
2023-05-10 23:47:35 +0900

    feature: 공지 쓰기 기능 서비스레이어 기능 추가

---
+++ src/main/kotlin/cbnu/io/cbnualramischeduler/business/web/notification/application/NotificationWriteCommand.kt
@@ -0,0 +1,6 @@
+package cbnu.io.cbnualramischeduler.business.web.notification.application
+
+interface NotificationWriteCommand {
+
+    fun scheduleSaveTask()
+}
---
+++ src/main/kotlin/cbnu/io/cbnualramischeduler/business/web/notification/application/service/NotificationWriteWriteService.kt
@@ -0,0 +1,32 @@
+package cbnu.io.cbnualramischeduler.business.web.notification.application.service
+
+import cbnu.io.cbnualramischeduler.business.core.domain.notification.Notification
+import cbnu.io.cbnualramischeduler.business.core.domain.notification.infrastructure.NotificationJpaRepository
+import cbnu.io.cbnualramischeduler.business.web.crawling.application.CbnuNotificationCrawler
+import cbnu.io.cbnualramischeduler.business.web.notification.application.NotificationWriteCommand
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+class NotificationWriteWriteService(
+    val notificationJpaRepository: NotificationJpaRepository,
+    val cbnuNotificationCrawler: CbnuNotificationCrawler,
+): NotificationWriteCommand {
+
+    @Transactional
+    override fun scheduleSaveTask() {
+        val crawlingNotification: MutableList<Notification> = cbnuNotificationCrawler.crawlingNotification()
+
+        for (notification in crawlingNotification) {
+            saveIfNotExistUrl(notification)
+        }
+
+    }
+
+    private fun saveIfNotExistUrl(notification: Notification) {
+        val existsByUrl = notificationJpaRepository.existsByUrl(notification.url)
+        if (!existsByUrl) {
+            notificationJpaRepository.save(notification)
+        }
+    }
+}
