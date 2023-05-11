c2d3cf894271990d638bfb433d6df392b88cdab2
Bangseungjae <tmdwo5825@naver.com>
2023-05-12 01:14:28 +0900

    build: 테스트 관련 의존성을 추가한다

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,11 +8,13 @@ plugins {
 	kotlin("plugin.jpa") version "1.6.21"
 	kotlin("plugin.lombok") version "1.8.21"
 	id("io.freefair.lombok") version "5.3.0"
+	id("io.kotest.multiplatform") version "5.0.2"
 }
 
 kotlinLombok {
 	lombokConfigurationFile(file("lombok.config"))
 }
+
 
 allOpen {
 	annotation("javax.persistence.Entity")
@@ -21,7 +23,7 @@ allOpen {
 }
 
 group = "cbnu.io"
-version = "0.0.1-SNAPSHOT"
+version = "5.5.5"
 java.sourceCompatibility = JavaVersion.VERSION_17
 
 repositories {
@@ -47,6 +49,12 @@ dependencies {
 
 	// Test
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation ("org.testcontainers:junit-jupiter:1.17.6")
+	testImplementation ("org.testcontainers:mysql:1.18.1")
+	testImplementation("io.kotest:kotest-runner-junit5:$version")
+	testImplementation("io.kotest:kotest-assertions-core:$version")
+	testImplementation("io.mockk:mockk:1.13.4")
+
 }
 
 tasks.withType<KotlinCompile> {
