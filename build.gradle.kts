2278a56254161b67ed6218c6cf76fb8707d2df58
Bangseungjae <tmdwo5825@naver.com>
2023-05-24 23:41:48 +0900

    build: log에 대한 오류를 수정한다

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,16 +3,10 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
 	id("org.springframework.boot") version "2.7.11"
 	id("io.spring.dependency-management") version "1.0.15.RELEASE"
+	id("io.kotest.multiplatform") version "5.0.2"
 	kotlin("jvm") version "1.6.21"
 	kotlin("plugin.spring") version "1.6.21"
 	kotlin("plugin.jpa") version "1.6.21"
-	kotlin("plugin.lombok") version "1.8.21"
-	id("io.freefair.lombok") version "5.3.0"
-	id("io.kotest.multiplatform") version "5.0.2"
-}
-
-kotlinLombok {
-	lombokConfigurationFile(file("lombok.config"))
 }
 
 
@@ -36,6 +30,9 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+//	implementation("io.github.microutils:kotlin-logging-jvm:3.0.5")
+	implementation("org.springframework.boot:spring-boot-starter-log4j2")
+
 
 	// Jpa
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
@@ -44,8 +41,10 @@ dependencies {
 	// MySQL
 	runtimeOnly("com.mysql:mysql-connector-j")
 
+	implementation ("org.springframework.boot:spring-boot-starter-data-redis")
+
 	// Crawling
-	implementation("org.jsoup:jsoup:1.11.3")
+	implementation("org.jsoup:jsoup:1.15.4")
 
 	// Test
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
@@ -53,7 +52,11 @@ dependencies {
 	testImplementation ("org.testcontainers:mysql:1.18.1")
 	testImplementation("io.kotest:kotest-runner-junit5:$version")
 	testImplementation("io.kotest:kotest-assertions-core:$version")
+//	testImplementation("io.kotest.extensions:kotest-extensions-testcontainers:$version")
 	testImplementation("io.mockk:mockk:1.13.4")
+	implementation("it.ozimov:embedded-redis:0.7.3") {
+		exclude("org.slf4j", "slf4j-simple")
+	}
 
 }
 
@@ -66,4 +69,18 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+
+configurations {
+	all {
+		exclude("org.springframework.boot:spring-boot-starter-logging")
+		exclude("ch.qos.logback:logback-classic:1.2.12")
+		exclude("org.apache.logging.log4j:log4j-to-sl4j")
+		exclude("org.springframework.boot:spring-boot-starter-logging")
+		exclude("org.jboss.logging:jboss-logging:3.4.3")
+		exclude("org.slf4j:sl4j-log4j12")
+		exclude("log4j:log4j")
+		exclude("org.slf4j", "slf4j-simple")
+		exclude("org.apache.logging.log4j", "log4j-slf4j-impl")
+	}
 }
